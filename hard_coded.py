c55df208fdd0cd63bfb2d99dadcfe746516787df
Himanshu deswal <36129430+dhimanshu07@users.noreply.github.com>
2019-06-30 02:17:53 +0100

    Chatbot using Machine Learning Concepts.
    Keras ,NLP , Recurrent Neural Network

---
+++ hard_coded.py
@@ -0,0 +1,139 @@
+
+## Here you can enter you hard coded response and reply
+
+hard_coded_response = ["ai", "ml", "louise brierty",
+                       "niall nurke",
+                       "edel chadwick",
+                       "padraig cooke",
+                       "kenneth corless",
+                       "martina cunningham",
+                       "caroline downes",
+                       "nuala doyle",
+                       "ronan dunbar",
+                       "joseph farrell",
+                       "sean glennon",
+                       "christoffer hannevig",
+                       "clement higginbotham",
+                       "joe lawless",
+                       "eoin mcIntyre",
+                       "martina nolan",
+                       "michael nugent",
+                       "patrick rogers",
+                       "sean lyons",
+                       "keith vaugh",
+                       "cyril barnicle",
+                       "john barrett",
+                       "tom bennett",
+                       "abcd",
+                       "declan byrne",
+                       "anthony commins",
+                       "mark daly",
+                       "frank doheny",
+                       "caroline downes",
+                       "sheila fallon",
+                       "enda fallon",
+                       "karol fitzgerald",
+                       "ronan flynn",
+                       "nigel flynn",
+                       "mary giblin",
+                       "paul jacob",
+                       "sean kennedy",
+                       "denis mcCarthy",
+                       "cormac mcClean",
+                       "kevin mcDermott",
+                       "james mooney",
+                       "niall murray",
+                       "michael rourke",
+                       "marcus rahilly",
+                       "michael russell",
+                       "sean lyons",
+                       "robert stewart",
+                       "michael thornton",
+                       "shivangi singh"]
+
+hard_coded_reply =["Artifical Intelligence", "Machine Learning",
+                    "Telephone: (090) 6442573    Email : lbrierty@ait.ie        Position: Faculty of Engineering & Informatics Administrator  ",
+                    "Telephone: (090) 6442573    Email : nburke@ait.ie          Position: Lecturer in Mechanical Engineering            Room :v305",
+                    "Telephone: (090) 6442573    Email : echadwick@ait.ie       Position: Lecturer in Polymer Technology	     Room :V301",
+                    "Telephone: (090) 6471845    Email : pcooke@ait.ie          Position: Lecturer in Control and Automation             Room :V303",
+                    "Telephone: (090) 6442538    Email : kcorless@ait.ie	Position: Lecturer in Mechanical & Polymer Engineering   Room :V303",
+                    "Email:mcunningham@ait.ie	Position:Lecturer in Mechanical & Polymer Engineering	  Room:U303",
+
+	            "Telephone:(090) 6468186	 Email:cdownes@ait.ie	        Position:Departmental Secretary of Engineering & Informatics",	
+	            "Telephone:(090) 6483060	 Email:ndoyle@ait.ie	        Position:Department Secretary in Trades	",
+		    "Telephone:(090) 6468174	 Email:rdunbar@ait.ie	        Position:Lecturer in Mechanical Engineering	          Room:X310",
+
+	            "Telephone:(090) 6468171	 Email:jfarrell@ait.ie	        Position:Lecturer in Mechanical & Polymer Engineering	          Room:V311",
+
+	            "Telephone:(090) 6468177	 Email:sglennon@ait.ie	        Position:Lecturer in Mechanical & Polymer Engineering	       Room:X310",
+	            "                            Email:channevig@ait.ie	        Position:Lecturer in Energy Management and Energy Auditing, Green Engineering, Sustainable EnergyRoom:Z305",
+
+		    "Telephone:(090) 6468050	Email:chigginbotham@ait.ie	Position:Lecturer in Chemistry/Polymer Materials/Biomedical Materials	          Room:RH211",
+
+		    "Telephone:(090) 6471824	Email:jlawless@ait.ie	        Position:Head of Department of Polymer, Mechanical, Civil Engineering & Trades	  Room:X307",
+
+		    "Telephone:(090) 6468176	Email:emcintyre@ait.ie         Position:Lecturer in Mechanical Engineering	Room:X310",
+
+	            "Email:mnolan@ait.ie	Position:Lecturer in Mechanical & Polymer Engineering	Room:U303",
+	            "Telephone:(090) 6468172	Email:mnugent@ait.ie	       Position:Lecturer in Mechanical & Polymer Engineering	Room:W305",
+
+	            "Telephone:(090) 6468182	Email:progers@ait.ie	       Position:Lecturer in Mechanical Engineering	Room:X310",
+
+		    "Telephone:0906468284	Email:slyons@ait.ie	       Position:Dean of Faculty of Engineering & Informatics	Room:X306",
+ 
+	            "Telephone:(090) 6442539	Email:kvaugh@ait.ie	       Position:Lecturer in Mechanical & Polymer Engineering	Room:V304",
+
+
+	            "Telephone:(090) 6468161	Email:cbarnicle@ait.ie	       Position:Lecturer	Room:U309",
+
+		    "Telephone:(090) 6468180	Email:jbarrett@ait.ie	       Position:Assistant Lecturer in Software Design and Game Development  Room:W305",
+		    "Telephone:(090) 6468185	Email:tbennett@ait.ie	       Position:Lecturer in Department of Electronics, Computer and Software Room:W305",
+	            "Telephone:(090) 6483098	Email:lbrierty@ait.ie	       Position:Faculty of Engineering & Informatics Administrator",
+
+		    "Telephone:(090) 6468166	dbyrne@ait.ie	              Position:Lecturer in Software Engineering	Room:U310",
+	            "Telephone:(090) 6471889	Email:acommins@ait.ie	      Position:Lecturer in Networks, Instrumentation and Computing  Room:U310",
+		    "Telephone:(090) 6468156	Email:mdaly@ait.ie	      Position:Lecturer",   
+		    "Telephone:(090) 6483013	Email:fdoheny@ait.ie	      Position:Mathematics Lecturer in school of Engineering	Room:U309",
+
+	            "Telephone:(090) 6468186	Email:cdownes@ait.ie	      Position:Departmental Secretary of Engineering & Informatics",
+
+		    "Telephone:(090) 6483012	Email:sheilafallon@ait.ie     Position:Lecturer in Software Engineering	   Room:X309",
+
+	            "Telephone:(090) 6471877	Email:efallon@ait.ie	      Position:Lecturer in Software Engineering	  Room:W305",
+ 
+
+		    "Telephone:(090) 6483034	Email:kfitzgerald@ait.ie      Position:Lecturer in Software Engineering	Room:Z304",
+
+		    "Telephone:(090) 6468164	Email:rflynn@ait.ie	      Position:Lecturer in Department of Electronics, Computer and Software   Room:U310",
+		    "Email:nflynn@ait.ie	Position:Lecturer in Department of Electronics, Computer and Software	Room:V311",
+ 
+	            "Telephone:(090) 6471888	Email:mgiblin@ait.ie	      Position:Lecturer in Department of Electronics, Computer and Software Engineering Room:X309",
+
+	            "Telephone:(090) 6468165	Email:pjacob@ait.ie	      Position:Lecturer in Department of Electronics, Computer and Software	Room:U310",
+		    "Telephone:(090) 6468177    Email:	sekennedy@ait.ie	      Position:Lecturer in Software Engineering	   Room:U310",
+	            "Telephone:(090) 6471841	Email:demccarthy@ait.ie       Position:Lecturer in Software Engineering	   Room:U302",
+                   "Telephone:(090) 6468157	Email:cmcclean@ait.ie	      Position:Lecturer in Electronic, Computer and Software Engineering	Room:U301",
+
+	            "Telephone:(090) 6468162	Email:kmcdermott@ait.ie	      Position:Lecturer in Department of Electronics, Computer and Software	Room:U309",
+		    "Telephone:(090) 6468189	Email:jamesmooney@ait.ie      Position:Lecturer in Department of Electronics, Computer and Software	Room:Y303",
+		    "Telephone:(090) 6468183	Email:nmurray@ait.ie	      Position:Lecturer, Department of Electronics, Computer & Software Engineering	Room:U309",
+		    "Telephone:(090) 643018	Email:miorourke@ait.ie        Position:Lecturer in Department of Electronics, Computer and Software Engineering	Room:U309",
+		    "Telephone:(090) 6468155	Email:mrahilly@ait.ie	      Position:Head of Department of Electronics & Informatics	Room:X304",
+		    "Telephone:(090) 6442541	Email:mrussell@ait.ie	      Position:Lecturer in Software Engineering and Learning & Teaching	Room:U302",
+	            "Telephone:0906468284	Email:slyons@ait.ie	      Position:Dean of Faculty of Engineering & Informatics	Room:X306",
+                    "Telephone:(090) 6471891	Email:rstewart@ait.ie	      Position:Lecturer in Electronic, Computer and Software Engineering	Room:X309",
+	
+	             "Telephone:(090) 6468160	Email:mthornton@ait.ie	     Position:Lecturer in Department of Electronics Computer and Software	Room:U304",
+
+                    "she is such a pretty girl and your an idiot"]
+
+
+def hard_reply(resp):
+    try:
+        index = hard_coded_response.index(resp)
+        reply = hard_coded_reply[index]
+    except Exception as e:
+        reply = None
+
+    return reply
+
